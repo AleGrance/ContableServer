853a5c8a8d87dcfb6e18256d265d6db18970eeda
System Administrator <alegrance1000@gmail.com>
2022-10-11 12:38:51 -0300

    Build 11102022

--- dist/routes/cabecera_compra.js
+++ dist/routes/cabecera_compra.js
@@ -1,5 +1,10 @@
 "use strict";
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+var _require = require("sequelize"),
+    Op = _require.Op;
+
 module.exports = function (app) {
   var Cabecera_compra = app.db.models.Cabecera_compra;
   var Contribuyente = app.db.models.Contribuyente;
@@ -64,6 +69,26 @@ module.exports = function (app) {
     Cabecera_compra.findAll({
       where: {
         ContribuyenteIdContribuyente: req.params.id
+      },
+      include: [{
+        model: Proveedor
+      }]
+    }).then(function (result) {
+      return res.json(result);
+    })["catch"](function (error) {
+      res.status(404).json({
+        msg: error.message
+      });
+    });
+  }) // Filtro en reporte
+  .post(function (req, res) {
+    console.log(req.body);
+    Cabecera_compra.findAll({
+      where: {
+        ContribuyenteIdContribuyente: req.params.id,
+        ProveedorIdProveedor: req.body.id_proveedor,
+        condicion_venta_compra: req.body.condicion,
+        fecha_factura_compra: _defineProperty({}, Op.between, [req.body.fecha_inicio, req.body.fecha_fin])
       },
       include: [{
         model: Proveedor
